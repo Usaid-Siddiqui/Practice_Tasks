a7840ccfdec9e133cfb58e5dbd60f439888a6a37
Usaid Siddiqui <usiddiqui@crimson.ua.edu>
2026-04-23 04:01:04 -0500

    finished linReg

--- src/linReg.cpp
+++ src/linReg.cpp
@@ -8,9 +8,30 @@ vector<vector<T>> matrixMultiply(const vector<vector<T>>& m1, const vector<vecto
 template<typename T>
 vector<vector<T>> transpose(const vector<vector<T>>& A);
 
+template<typename T>
+vector<vector<T>> linearRegression(const vector<vector<T>>& X, const vector<vector<T>>& y);
+
 
 int main() {
+    vector<vector<double>> X = {
+        {1, 1000, 2},
+        {1, 1500, 3},
+        {1, 1200, 2},
+        {1, 2000, 4}
+    };
 
+    vector<vector<double>> y = {
+        {200},
+        {300},
+        {240},
+        {450}
+    };
+
+    auto b = linearRegression(X, y);
+
+    cout << "Intercept:          " << b[0][0] << "\n";
+    cout << "Price per sqft:     " << b[1][0] << "\n";
+    cout << "Price per bedroom:  " << b[2][0] << "\n";
 
     return 0;
 }
@@ -35,7 +56,7 @@ vector<vector<T>> matrixMultiply(const vector<vector<T>>& m1, const vector<vecto
 template<typename T>
 vector<vector<T>> transpose(const vector<vector<T>>& A) {
     int m = A.size(), n = A[0].size();
-    vector<vector<T> B(n,vector<T>(m,0));
+    vector<vector<T>> B(n,vector<T>(m,0));
 
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
@@ -50,7 +71,7 @@ vector<vector<T>> findInverse(const vector<vector<T>>& A) {
 
     // build augmented matrix
     vector<vector<T>> aug(n, vector<T>(2*n,0));
-    for int (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
             aug[i][j] = A[i][j];
         aug[i][i+n] = 1;
@@ -58,6 +79,48 @@ vector<vector<T>> findInverse(const vector<vector<T>>& A) {
 
     // elimination
     for (int col = 0; col < n; col++) {
-        
+        // find pivot, the largest absolute element in the column
+        int pivot = col;
+        for (int row = col + 1; row < n; row++) {
+            if (abs(aug[row][col]) > abs(aug[pivot][col])) {
+                pivot = row;
+            }
+        }
+
+        swap(aug[col], aug[pivot]);
+        // if the largest value in the column is 0, matrix is non invertable
+        if (aug[col][col] == 0) {
+            cout << "Matrix is singular, cannot invert." << endl;
+            return {};
+        }
+
+        T scale = aug[col][col];
+        for (int j = 0; j < 2*n; j++) {
+            aug[col][j] /= scale;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (row == col) continue;
+            T factor = aug[row][col];
+            for (int j = 0; j < 2 * n; j++)
+                aug[row][j] -= factor * aug[col][j];
+        }
     }
+
+    // Right half of matrix is now inverse
+    vector<vector<T>> inv(n, vector<T>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            inv[i][j] = aug[i][j + n];
+
+    return inv;
+}
+
+template<typename T>
+vector<vector<T>> linearRegression(const vector<vector<T>>& X, const vector<vector<T>>& y) {
+    auto Xt     = transpose(X);
+    auto XtX    = matrixMultiply(Xt, X);
+    auto XtXinv = findInverse(XtX);       // use Gauss-Jordan for stability
+    auto Xty    = matrixMultiply(Xt, y);
+    return matrixMultiply(XtXinv, Xty);
 }
